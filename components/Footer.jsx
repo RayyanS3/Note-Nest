9eaab08b455f693bcd9c749685213fdcce10350e
Rayyan Suhail <suhailr@mcmaster.ca>
2023-10-07 21:07:30 -0400

    Header and Footer .jsx updated

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const date = new Date();
+const year = date.getFullYear();
+
+function Footer() {
+  return <Footer>Copyright {year}</Footer>;
+}
+
+export default Footer;
